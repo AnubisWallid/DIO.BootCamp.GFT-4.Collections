457d4da85e6eedd57a0fb0fe63efbb2846eddd7b
AnubisWallid <wallidmetal@hotmail.com>
2022-05-24 12:08:44 -0300

    Exercicio Map

---
+++ src/ExemploMap.java
@@ -0,0 +1,29 @@
+
+/*
+ Dada os modelos dos carros e seus respectivos consumos na estrada, faça:
+ modelo = gol - consumo = 14,4km/l
+ modelo = uno - consumo = 15,6 km/l
+ modelo = mobi - consumo = 16,1 km/l
+ modelo = hb20 - consumo = 14,5 km/l
+ modelo = kwid - consumo = 15,6 km/l
+ */
+public class ExemploMap {
+    public static void main(String[] args) {
+        System.out.println("Crie um dicionário que relacione os modelos e seus respectivos consumos: ");
+        System.out.println("Substitua o consumo do gol por 15,2 km/l: ");
+        System.out.println("Confira se o modelo tucson está no dicionário: ");
+        System.out.println("Exiba o consumo do uno: ");
+        System.out.println("Exiba o terceiro modelo adicionado: ");
+        System.out.println("Exiba os modelos: ");
+        System.out.println("Exiba os consumos dos carros: ");
+        System.out.println("Exiba o modelo mais econômico e seu consumo: ");
+        System.out.println("Exiba o modelo menos econômico e seu consumo: " );
+        System.out.println("Exiba a soma dos consumos: ");
+        System.out.println("Exiba a média dos consumos deste dicionário de carros: ");
+        System.out.println("Remova os modelos com o consumo igual a 15,6 km/l: ");
+        System.out.println("Exiba todos os carros na ordem em que foram informados: ");
+        System.out.println("Exiba o dicionário ordenado pelo modelo: ");
+        System.out.println("Apague o dicionario de carros: ");
+        System.out.println("Confira se o dicionário está vazio: ");
+    }
+}
