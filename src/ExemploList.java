71f9c3e503d5a3922a6ceb131a4012022c470eef
AnubisWallid <wallidmetal@hotmail.com>
2022-05-23 21:54:20 -0300

    Resolução do Exercicio

--- src/ExemploList.java
+++ src/ExemploList.java
@@ -1,50 +1,101 @@
 //Dada uma lista com 7 notas de um aluno [7, 8.5, 9.3, 5, 7, 0, 3.6], faça:
 
+import java.util.*;
+
 class ExemploList {
     public static void main(String[] args) {
 
         System.out.println("Crie uma lista e adicione as sete notas: ");
+        /* ALGUMAS FORMAS DE INICIAR A LIST :
+        List notas = new ArrayList(); //antes do java 5
+        ArrayList<Double> notas = new ArrayList<>();
+        List<Double> notas = new ArrayList<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
+        List<Double> notas = Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+        notas.add(10d);
+        System.out.println(notas);
+        List<Double> notas = List.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+        notas.add(1d); // ERRO
+        notas.remove(5d); // ERRO
+        */
+        List<Double> notas = new ArrayList<>(); //Generics(jdk 5) - Diamond Operator(jdk 7)
+        notas.add(7.0);
+        notas.add(8.5);
+        notas.add(9.3);
+        notas.add(5.0);
+        notas.add(7.0);
+        notas.add(0.0);
+        notas.add(3.6);
+
 
         System.out.println("Exiba a posição da nota 5.0: ");
+        System.out.println(notas.indexOf(5.0));
 
         System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
+        notas.add(3, 8.0);
+        System.out.println(notas);
 
         System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
+        notas.set(notas.indexOf(5.0), 8.0d);
+        System.out.println(notas);
 
-        System.out.println("Confira se a nota 5.0 está na lista: ");
+        System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5.0));
 
         System.out.println("Exiba todas as notas na ordem em que foram informados: ");
+        System.out.println(notas);
 
-        System.out.println("Exiba a terceira nota adicionada: ");
+        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
 
-        System.out.println("Exiba a menor nota: ");
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
 
-        System.out.println("Exiba a maior nota: ");
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
 
         System.out.println("Exiba a soma dos valores: ");
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while (iterator.hasNext()) {
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("Soma de Notas: " + soma);
 
-        System.out.println("Exiba a média das notas: ");
+        System.out.println("Exiba a média das notas: " + (soma / notas.size()));
 
         System.out.println("Remova a nota 0: ");
+        notas.remove(0.0);
+        System.out.println(notas);
 
         System.out.println("Remova a nota da posição 0");
+        notas.remove(0);
+        System.out.println(notas);
 
         System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+        /*Iterator<Double> iterator2 = notas.iterator();
+         while(iterator2.hasNext()){
+            Double num = iterator2.next();
+            if(num < 7D) iterator2.remove();
+        }*/
+        notas.removeIf(num -> num < 7D); // DICA DO INTELLIJ
+
+        System.out.println(notas);
 
         System.out.println("Apague toda a lista");
+//        notas.clear();
+        System.out.println(notas);
 
-        System.out.println("Confira se a lista está vazia: ");
+        System.out.println("Confira se a lista está vazia: " + notas.isEmpty());
 
-/*
-Para você: Resolva esses exercícios utilizando os métodos da implementação LinkedList:
+
+        //Para você: Resolva esses exercícios utilizando os métodos da implementação LinkedList:
 
         System.out.println("Crie uma lista chamada notas2 " +
                 "e coloque todos os elementos da list Arraylist nessa nova lista: ");
+        LinkedList<Double> notasLinked = new LinkedList<>(notas);
+        System.out.println(notasLinked);
 
-        System.out.println("Mostre a primeira nota da nova lista sem removê-lo: ");
+        System.out.println("Mostre a primeira nota da nova lista sem removê-lo: " + notasLinked.getFirst());
+        System.out.println(notasLinked);
 
-        System.out.println("Mostre a primeira nota da nova lista removendo-o: ");
-*/
-
+        System.out.println("Mostre a primeira nota da nova lista removendo-o: " + notasLinked.removeFirst());
+        System.out.println(notasLinked);
     }
 }
