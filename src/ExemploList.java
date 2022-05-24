5a0f669fe8b554fce3ba50634616bfa156cfa9ec
AnubisWallid <wallidmetal@hotmail.com>
2022-05-23 22:02:40 -0300

    Início do Exercicio de Ordenação

--- src/ExemploList.java
+++ src/ExemploList.java
@@ -11,7 +11,7 @@ class ExemploList {
         ArrayList<Double> notas = new ArrayList<>();
         List<Double> notas = new ArrayList<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
         List<Double> notas = Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
-        notas.add(10d);
+        notas.add(10d); // ERRO
         System.out.println(notas);
         List<Double> notas = List.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
         notas.add(1d); // ERRO
