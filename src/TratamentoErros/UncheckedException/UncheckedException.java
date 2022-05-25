4f76df46d4253796e27474ec01d7c8b1a6feb23d
AnubisWallid <wallidmetal@hotmail.com>
2022-05-25 14:34:22 -0300

    UncheckedExceptio - Programa Sem Erro

--- src/TratamentoErros/UncheckedException/UncheckedException.java
+++ src/TratamentoErros/UncheckedException/UncheckedException.java
@@ -4,14 +4,35 @@ import javax.swing.*;
 
 public class UncheckedException {
     public static void main(String[] args) {
-        String num1 = JOptionPane.showInputDialog("Numerador: ");
-        String num2 = JOptionPane.showInputDialog("Denominador: ");
+        String num1;
+        String num2;
+        boolean doWhile = true;
+        do {
 
-        int resultado = dividir(Integer.parseInt(num1),Integer.parseInt(num2));
-        System.out.println("Resultado: "+ resultado);
-    }
+                try {
+                    num1 = JOptionPane.showInputDialog("Numerador: ");
+                    num2 = JOptionPane.showInputDialog("Denominador: ");
+                    double resultado = dividir(Double.parseDouble(num1), Double.parseDouble(num2));
+                    System.out.println("Resultado: " + resultado);
+                    doWhile = false;
+                }catch (NumberFormatException exceptionNoResultado){
+        //            exceptionNoResultado.printStackTrace();
+                    JOptionPane.showMessageDialog(null,"Entrada invalida.\n"+ exceptionNoResultado.getMessage());
+                }catch (ArithmeticException exceptionNaFuncaoDividir){
+                    JOptionPane.showMessageDialog(null,"Impossivel dividir por 0.\n"+ exceptionNaFuncaoDividir.getMessage());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    System.out.println("Dentro do bloco finally");
+                }
+            }while (doWhile);
 
-    private static int dividir(int num1, int num2) {
-        return num1 / num2;
+        System.out.println("fora do DO/While...\nContinua...");
+
     }
+        private static double dividir ( double num1, double num2){
+        if(num2 == 0){
+            throw new ArithmeticException();}
+            return Math.floor(num1 / num2);
+        }
 }
